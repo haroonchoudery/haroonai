cf21d48443dd37bbe863dd36912b658e39884513
Haroon Choudery <haroon.choudery@gmail.com>
2024-03-31 12:41:49 -0400

    logo fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Link from "next/link";
 // import { Suspense } from "react";
 
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+    />
+  );
+}
+
 function ArrowIcon() {
   return (
     <svg
@@ -49,29 +59,50 @@ export default function Page() {
       </h1>
       <p className="prose prose-neutral dark:prose-invert">
         I lead the team at{" "}
-        <Link href="https://autoblocks.ai">Autoblocks AI</Link>, where we're
-        working towards AI product alignment.
+        <span className="not-prose">
+          <Badge href="https://autoblocks.ai/">
+            <svg
+              width="13"
+              height="15"
+              role="img"
+              aria-label="Autoblocks logo"
+              className="mr-1 inline-flex"
+            >
+              <use href="/sprite.svg#autoblocks" />
+            </svg>
+            Autoblocks AI
+          </Badge>
+        </span>
+        , where we're working towards AI product alignment. Here's a summary of
+        my career so far:
         <br />
         <br />
-        Previously, I worked in ML / data science roles at Mark Cuban Companies,
-        Deloitte, and Komodo Health, and led growth at startups like Hex and
-        Highlight.
+        <strong>Work</strong>: I worked in ML / data science roles at Mark Cuban
+        Companies, Deloitte, and Komodo Health, and led growth marketing at
+        startups like Hex and Highlight.
         <br />
         <br />
-        In 2015, I founded AI For Anyone, one of the nation&apos;s most
-        impactful AI education nonprofits, and Not A Bot, an AI newsletter with
-        50k+ subscribers.
+        <strong>School</strong>: I'm a UC Berkeley masters grad in data science,
+        with a focus on ML. I'm also a proud first-gen college grad.
+        <br />
+        <br />
+        <strong>Other</strong>: I founded AI For Anyone, one of the
+        nation&apos;s most impactful AI education nonprofits, and Not A Bot, an
+        AI newsletter with 50k+ subscribers.
       </p>
       <div className="my-8 flex w-full flex-col space-y-4">
         <BlogLink
           name="What is AI product alignment?"
-          slug="ai-product-alignment"
+          slug="what-is-ai-product-alignment"
         />
         <BlogLink
           name="Are we missing the point of evaluators?"
           slug="evaluators"
         />
-        <BlogLink name="How do we get to AI product alignment" slug="heroku" />
+        <BlogLink
+          name="How do we get to AI product alignment"
+          slug="path-to-ai-product-alignment"
+        />
       </div>
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
